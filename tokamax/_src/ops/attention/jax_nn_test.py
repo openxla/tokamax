Attention test reference precision fixes.
 - Always perform reference in at least f32 precision.
 - Fix `dout` precision in reference (downcast to impl precision).
 - Apply `logits_dtype` in reference for `test_logits_dtype`.
 - Logit type fixes in `XlaChunkedAttention`.

PiperOrigin-RevId: 807652704

tokamax/_src/ops/attention/jax_nn_test.py
@@ -49,7 +49,7 @@ class JaxNnDotProductAttentionTest(test_base.AttentionTestBase):
       kwargs["expect_supported"] = False
     super()._run_test_with_inputs(*args, **kwargs)
 
-  def test_bench_deepseek2_16b_bf16(self, *_):
+  def test_bench_deepseek2_16b_bf16(self):
     self.skipTest("Test OOMs with JaxNNDotProductAttention implementations.")
 
 
@@ -75,25 +75,14 @@ class JaxNnDotProductAttentionCudnnTest(JaxNnDotProductAttentionTest):
       cast = lambda x: None if x is None else x.astype(jnp.bfloat16)
       return orig_impl(cast(q), cast(k), cast(v), bias=cast(bias), **kwargs)
 
-    atol = 0.025 if "bias" in kwargs else 0.015
-    super()._run_test_with_inputs(*args, **kwargs | dict(impl=impl, atol=atol))
-
-  @parameterized.parameters(False, True)
-  def test_flax_mha_compatibility(self, broadcast_dropout):
-    del broadcast_dropout  # Unused.
-    self.skipTest("fp32 dtype not supported.")
+    kwargs["impl"] = impl
+    kwargs["atol"] = 0.025 if "bias" in kwargs else 0.015
+    if args[0].shape[-1] > 128:
+      kwargs["expect_supported"] = False
+    super()._run_test_with_inputs(*args, **kwargs)
 
   def test_padding_mask_with_nans(self):
     self.skipTest("Unsupported.")
-
-  @parameterized.parameters(
-      *test_base.base_names_and_params("test_quantized_int4")
-  )
-  def test_quantized_int4(self, test_name, kwargs):
-    self.skipTest("Unsupported head_dim > 128.")
-
-  def test_bench_veo3(self, *_):
-    self.skipTest("Unsupported head_dim > 128.")
 
   @parameterized.parameters(*test_base.base_names_and_params("test_vmap"))
   def test_vmap(self, test_name, kwargs):

tokamax/_src/ops/attention/pallas_mosaic_gpu_flash_attention_test.py
@@ -67,7 +67,6 @@ class PallasMosaicGpuFlashAttentionTest(test_base.AttentionTestBase):
     impl_kwargs = kwargs.setdefault("impl_kwargs", {})
     impl_kwargs["precision"] = jax.lax.DotAlgorithmPreset.DEFAULT
     impl_kwargs["logits_dtype"] = jnp.float32
-    ref_impl = kwargs.get("ref_impl", test_base.nn.dot_product_attention)
 
     def as_bf16(x):
       if isinstance(x, jax.Array) and x.dtype == jnp.float32:
@@ -75,17 +74,6 @@ class PallasMosaicGpuFlashAttentionTest(test_base.AttentionTestBase):
       return x
 
     q, k, v, bias = map(as_bf16, (q, k, v, bias))
-
-    def wrapped_ref_impl(q, k, v, *, bias=None, **kwargs):
-      def as_f32(x):
-        if isinstance(x, jax.Array):
-          return x.astype(jnp.promote_types(x.dtype, jnp.float32))
-        return x
-
-      q, k, v, bias = map(as_f32, (q, k, v, bias))
-      return ref_impl(q, k, v, bias=bias, **kwargs)
-
-    kwargs["ref_impl"] = wrapped_ref_impl
     kwargs["atol"] = 0.0045 if bias is None else 0.007
 
     if bias is not None or not impl_kwargs.get("normalize_output", True):
@@ -95,7 +83,7 @@ class PallasMosaicGpuFlashAttentionTest(test_base.AttentionTestBase):
 
   def test_causal_mask(self):
     # TODO: Investigate why it's less accurate with causal mask.
-    with test_base.override_test_args(atol=0.006, atol_grads=0.025):
+    with test_base.override_test_args(atol=0.006, atol_grads=0.015):
       super().test_causal_mask()
 
   def test_causal_mask_cross_attention0(self):

tokamax/_src/ops/attention/test_base.py
@@ -118,8 +118,18 @@ def _run_test(
     impl = apply_output_mask(impl)
     ref_impl = apply_output_mask(ref_impl)
 
+  # Upcast all floating point inputs to f32 for reference implementation.
+  def f32_ref_impl(*args, _ref_impl=ref_impl, **kwargs):
+    def as_f32(x):
+      if isinstance(x, jax.Array) and jnp.issubdtype(x.dtype, jnp.floating):
+        return x.astype(jnp.promote_types(x.dtype, jnp.float32))
+      return x
+
+    args, kwargs = jax.tree.map(as_f32, (args, kwargs))
+    return _ref_impl(*args, **kwargs)
+
   impl = functools.partial(impl, **kwargs, **impl_kwargs)
-  ref_impl = functools.partial(ref_impl, **kwargs, **ref_kwargs)
+  ref_impl = functools.partial(f32_ref_impl, **kwargs, **ref_kwargs)
 
   # Forwards inference.
   actual = jax.jit(impl)(q, k, v, bias=bias)
@@ -141,17 +151,16 @@ def _run_test(
 
   if rng is None:
     rng = jax.random.PRNGKey(42)
-  dout = jax.random.normal(rng, expected.shape, dtype=expected.dtype)
+  dout = jax.random.normal(rng, expected.shape, dtype=actual.dtype)
+  ref_dout = dout.astype(expected.dtype)
 
   # Backwards.
   if atol_grads is None:
     atol_grads = max(2 * atol, 5e-6)
 
   grad_names = ("dq", "dk", "dv", "dbias")
-  actual_grads = dict(
-      zip(grad_names, vjp_fn(dout.astype(actual.dtype)), strict=True)
-  )
-  expected_grads = dict(zip(grad_names, ref_vjp_fn(dout), strict=True))
+  actual_grads = dict(zip(grad_names, vjp_fn(dout), strict=True))
+  expected_grads = dict(zip(grad_names, ref_vjp_fn(ref_dout), strict=True))
   rtol_grads = max(atol_grads / 10, 1e-6)
   chex.assert_trees_all_close(
       actual_grads, expected_grads, atol=atol_grads, rtol=rtol_grads
@@ -159,7 +168,7 @@ def _run_test(
 
   if test_vjp_deterministic:
     actual2, vjp_fn = jax.vjp(wrap(impl), q, k, v, bias)
-    actual_grads2 = dict(zip(grad_names, vjp_fn(dout.astype(actual.dtype))))
+    actual_grads2 = dict(zip(grad_names, vjp_fn(dout)))
     chex.assert_trees_all_equal(actual, actual2)
     chex.assert_trees_all_equal(actual_grads, actual_grads2)
 
@@ -182,17 +191,15 @@ def _ref_impl_tanh(
 ):
   """Reference implementation for tanh attention."""
   q /= jnp.sqrt(q.shape[-1])
-  attn_weights = jnp.einsum("...qhd,...khd->...hqk", q, k, precision=precision)
+  logits = jnp.einsum("...qhd,...khd->...hqk", q, k, precision=precision)
   if bias is not None:
-    attn_weights += bias
-  attn_weights = logits_soft_cap * jnp.tanh(attn_weights / logits_soft_cap)  # pytype: disable=unsupported-operands,wrong-arg-types  # jax-operator-types
+    logits += bias
+  if logits_soft_cap is not None:
+    logits = logits_soft_cap * jnp.tanh(logits / logits_soft_cap)
   if mask is not None:
-    min_value = float(jnp.finfo(attn_weights.dtype).min)
-    attn_weights = jnp.where(mask, attn_weights, min_value)
-  attn_weights = jax.nn.softmax(attn_weights)
-  return jnp.einsum(
-      "...hqk,...khd->...qhd", attn_weights, v, precision=precision
-  )
+    logits = jnp.where(mask, logits, float(jnp.finfo(logits.dtype).min))
+  weights = jax.nn.softmax(logits)
+  return jnp.einsum("...hqk,...khd->...qhd", weights, v, precision=precision)
 
 
 # pylint: disable=missing-function-docstring
@@ -331,7 +338,6 @@ class AttentionTestBase(parameterized.TestCase):
         (2, 1024, 4, 64),
         bias_shape=bias_shape,
         atol=5e-6,
-        atol_grads=2e-5,
         expect_supported=self._supports_bias,
     )
 
@@ -670,7 +676,7 @@ class AttentionTestBase(parameterized.TestCase):
         (2, 1024, 4, 64),
         impl_kwargs=dict(logits_dtype=dtype),
         dtype=dtype,
-        atol=({"float16": 5e-4, "bfloat16": 5e-3})[dtype],
+        atol=({"float16": 1e-3, "bfloat16": 1e-2})[dtype],
         expect_supported=self._supports_logits_dtype,
     )
 
@@ -686,9 +692,7 @@ class AttentionTestBase(parameterized.TestCase):
         (2, 1024, 4, 64),
         impl_kwargs=dict(logits_scale=1.0, normalize_output=False),
         ref_impl=ref_impl,
-        dtype=jnp.float32,
         atol=1e-5,
-        atol_grads=3e-5,
     )
 
   def test_partial_attention(self):
@@ -795,8 +799,8 @@ class AttentionTestBase(parameterized.TestCase):
         self._supports_vjp and not is_quantized and q.shape[-2] == k.shape[-2]
     )
 
-    atol = {jnp.float32: 2e-6, jnp.bfloat16: 5e-2}[q.dtype.type]
-    atol_grads = {jnp.float32: 2e-5, jnp.bfloat16: 0.3}[q.dtype.type]
+    atol = {jnp.float32: 2e-6, jnp.bfloat16: 2e-2}[q.dtype.type]
+    atol_grads_bias = {jnp.float32: 2e-5, jnp.bfloat16: 5e-2}[q.dtype.type]
     try:
       self._run_test_with_inputs(
           q,
@@ -810,7 +814,7 @@ class AttentionTestBase(parameterized.TestCase):
           ref_impl=ref_impl_,
           ref_kwargs=ref_kwargs,
           atol=atol,
-          atol_grads=None if bias is None else atol_grads,
+          atol_grads=None if bias is None else atol_grads_bias,
           expect_supported=expect_supported,
           test_vjp=test_vjp,
       )

tokamax/_src/ops/attention/xla_chunked.py
@@ -69,7 +69,7 @@ def _attend_chunk(
   logits *= logits_scale
 
   if bias is not None:
-    logits += bias
+    logits += bias.astype(logits.dtype)
 
   if logits_soft_cap is not None:
     logits = logits_soft_cap * jnp.tanh(logits / logits_soft_cap)
@@ -78,6 +78,7 @@ def _attend_chunk(
     mask_value = float(jnp.finfo(logits.dtype).min)
     logits = jnp.where(jnp.asarray(mask), logits, mask_value)
 
+  logits = logits.astype(jnp.promote_types(logits.dtype, jnp.float32))
   loc_x_max = jnp.max(logits, axis=-1)
   new_x_max = jnp.maximum(x_max, loc_x_max)
   weights = jnp.exp(logits - new_x_max[..., None])