3aff66a45697d836a75565638da300ad96ee1a55
Sebastian Bodenstein <sbodenstein@google.com>
2025-10-17 16:09:54 -0700

    Clean up triangle multiplication tests.
    PiperOrigin-RevId: 820862506

--- tokamax/_src/ops/triangle_multiplication/api_test.py
+++ tokamax/_src/ops/triangle_multiplication/api_test.py
@@ -14,13 +14,12 @@
 # ==============================================================================
 import typing
 from typing import Final
-
 from absl.testing import absltest
 from absl.testing import parameterized
 import jax
 import jax.numpy as jnp
-
 from tokamax._src import jaxtyping
+from tokamax._src import numerics
 from tokamax._src.ops.triangle_multiplication import api
 
 _IMPLEMENTATIONS: Final[tuple[str | None, ...]] = typing.get_args(
@@ -29,29 +28,18 @@ _IMPLEMENTATIONS: Final[tuple[str | None, ...]] = typing.get_args(
 
 
 def _get_params(n, c, h, d, dtype):
-  keys = jax.random.split(jax.random.PRNGKey(0), 10)
-  x = jax.random.normal(keys[0], (n, n, c), dtype=dtype)
-  mask = jax.random.bernoulli(keys[1], shape=(n, n))
-  gate_projection_weights = jax.random.normal(
-      keys[2], (c, 2, h, 2), dtype=dtype
+  params = dict(
+      x=jax.ShapeDtypeStruct((n, n, c), dtype=dtype),
+      mask=jax.ShapeDtypeStruct((n, n), dtype=jnp.bool),
+      gate_projection_weights=jax.ShapeDtypeStruct((c, 2, h, 2), dtype=dtype),
+      projection_out_weights=jax.ShapeDtypeStruct((h, d), dtype=dtype),
+      gate_out_weights=jax.ShapeDtypeStruct((c, d), dtype=dtype),
+      layernorm_in_scale=jax.ShapeDtypeStruct((c,), dtype=dtype),
+      layernorm_in_offset=jax.ShapeDtypeStruct((c,), dtype=dtype),
+      layernorm_out_scale=jax.ShapeDtypeStruct((h,), dtype=dtype),
+      layernorm_out_offset=jax.ShapeDtypeStruct((h,), dtype=dtype),
   )
-  projection_out_weights = jax.random.normal(keys[3], (h, d), dtype=dtype)
-  gate_out_weights = jax.random.normal(keys[4], (c, d), dtype=dtype)
-  layernorm_in_scale = jax.random.normal(keys[5], (c,), dtype=dtype)
-  layernorm_in_offset = jax.random.normal(keys[6], (c,), dtype=dtype)
-  layernorm_out_scale = jax.random.normal(keys[7], (h,), dtype=dtype)
-  layernorm_out_offset = jax.random.normal(keys[8], (h,), dtype=dtype)
-  return (
-      x,
-      mask,
-      gate_projection_weights,
-      projection_out_weights,
-      gate_out_weights,
-      layernorm_in_scale,
-      layernorm_in_offset,
-      layernorm_out_scale,
-      layernorm_out_offset,
-  )
+  return numerics.random_initialize(params)
 
 
 class TriangleMultiplicationTest(parameterized.TestCase):
@@ -62,27 +50,26 @@ class TriangleMultiplicationTest(parameterized.TestCase):
       implementation=_IMPLEMENTATIONS,
   )
   def test_triangle_multiplication(self, triangle_type, dtype, implementation):
-    n, c, h, d = 8, 16, 32, 64
-    x, *params = _get_params(n, c, h, d, dtype)
+    n = 8
+    d = 64
+    params = _get_params(n=n, c=16, h=32, d=d, dtype=dtype)
 
     @jax.jit
-    def f(x, *args):
+    def f(params):
       return api.triangle_multiplication(
-          x, *args, triangle_type=triangle_type, implementation=implementation
+          **params, triangle_type=triangle_type, implementation=implementation
       )
 
-    out = f(x, *params)
+    out = f(params)
     self.assertEqual(out.shape, (n, n, d))
     self.assertEqual(out.dtype, dtype)
 
   def test_unsupported_implementation(self):
-    n, c, h, d = 8, 16, 32, 64
-    x, *params = _get_params(n, c, h, d, jnp.float32)
+    params = _get_params(n=8, c=16, h=32, d=64, dtype=jnp.float32)
     with self.assertRaisesRegex(NotImplementedError, "Only XLA"):
       with jaxtyping.disable_jaxtyping():
         api.triangle_multiplication(
-            x,
-            *params,
+            **params,
             triangle_type="incoming",
             implementation="unsupported",
         )
