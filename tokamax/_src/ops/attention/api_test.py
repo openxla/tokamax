When BF16 arrays are broadcast, the backwards pass can do the resulting reduction in BF16. This fixes issues in layer_norm and attention.
PiperOrigin-RevId: 826480278

tokamax/_src/ops/attention/api_test.py
@@ -24,6 +24,7 @@ from tokamax import autotuning
 from tokamax._src import batching
 from tokamax._src import jaxtyping
 from tokamax._src import mosaic_gpu
+from tokamax._src import shape as shape_lib
 from tokamax._src import triton as triton_lib
 from tokamax._src.ops.attention import api
 
@@ -62,8 +63,10 @@ class DotProductAttentionTest(parameterized.TestCase):
 
     # For testing purposes, we call the non-GQA version without vmap in the
     # reference code
-    K_ref = jnp.repeat(K, G, axis=2)
-    V_ref = jnp.repeat(V, G, axis=2)
+    with shape_lib.upcast_broadcast():
+      K_ref = jnp.repeat(K, G, axis=2)
+      V_ref = jnp.repeat(V, G, axis=2)
+
     out_ref, sdpa_vjp_ref = jax.vjp(sdpa_ref, Q, K_ref, V_ref, bias, mask)
     out_ans, sdpa_vjp_ans = jax.vjp(sdpa_ans, Q, K, V, bias, mask)
 

tokamax/_src/ops/attention/base.py
@@ -19,7 +19,6 @@ import functools
 import math
 import types
 from typing import Any, Literal, NotRequired, Self, TypeVar, TypedDict, overload
-
 import jax
 from jax import export
 from jax.experimental import shard_map
@@ -30,6 +29,7 @@ from tokamax._src import ad
 from tokamax._src import jaxtyping
 from tokamax._src import precision as precision_lib
 from tokamax._src import quantization
+from tokamax._src import shape as shape_lib
 from tokamax._src.ops import op
 from typing_extensions import override
 
@@ -590,8 +590,10 @@ class DotProductAttention(
     q, k, v = map(as_array, (q, k, v))
     if k.shape[-2] not in (1, q.shape[-2]):
       repeats = q.shape[-2] // k.shape[-2]
-      k = jnp.repeat(k, repeats, axis=-2)
-      v = jnp.repeat(v, repeats, axis=-2)
+
+      with shape_lib.upcast_broadcast():
+        k = jnp.repeat(k, repeats, axis=-2)
+        v = jnp.repeat(v, repeats, axis=-2)
 
     q_k_dot_precision, weights_v_dot_precision = precision
 

tokamax/_src/ops/attention/xla_chunked.py
@@ -20,7 +20,6 @@ gives more similar numerics to FlashAttention.
 
 import dataclasses
 import functools
-
 import jax
 import jax.numpy as jnp
 from jaxtyping import Array, Bool, Float, Int  # pylint: disable=g-multiple-import,g-importing-member
@@ -343,8 +342,10 @@ class XlaChunkedDotProductAttention(
     q, k, v = map(base.as_array, (q, k, v))
     if k.shape[-2] not in (1, q.shape[-2]):
       repeats = q.shape[-2] // k.shape[-2]
-      k = jnp.repeat(k, repeats, axis=-2)
-      v = jnp.repeat(v, repeats, axis=-2)
+
+      with shape_lib.upcast_broadcast():
+        k = jnp.repeat(k, repeats, axis=-2)
+        v = jnp.repeat(v, repeats, axis=-2)
 
     is_paged = paging_info is not None
     single_chunk = isinstance(self.chunk_size, int)

tokamax/_src/ops/flex_attention/base.py
@@ -17,13 +17,13 @@
 from collections.abc import Callable
 import functools
 from typing import Any, ClassVar, Literal, TypeVar, overload
-
 import jax
 import jax.numpy as jnp
 from jaxtyping import Array, Bool, Float  # pylint: disable=g-multiple-import,g-importing-member
 from tokamax._src import jaxtyping
 from tokamax._src import precision as precision_lib
 from tokamax._src import quantization
+from tokamax._src import shape as shape_lib
 from tokamax._src.ops import op
 from typing_extensions import override
 
@@ -44,6 +44,7 @@ class FlexAttention(
     op.Op[Any, Float[Array, "*B T H d"], Residuals, _Config, _Key]
 ):
   """FlexAttention function."""
+
   supports_batched_args_capture: ClassVar[bool] = False
 
   # We override `__call__` in order to handle sharding at the top level.
@@ -257,8 +258,10 @@ class FlexAttention(
     q, k, v = map(as_array, (q, k, v))
     if k.shape[-2] not in (1, q.shape[-2]):
       repeats = q.shape[-2] // k.shape[-2]
-      k = jnp.repeat(k, repeats, axis=-2)
-      v = jnp.repeat(v, repeats, axis=-2)
+
+      with shape_lib.upcast_broadcast():
+        k = jnp.repeat(k, repeats, axis=-2)
+        v = jnp.repeat(v, repeats, axis=-2)
 
     q_k_dot_precision, weights_v_dot_precision = precision
 

tokamax/_src/ops/flex_attention/test_base.py
@@ -18,6 +18,7 @@ from absl.testing import absltest
 import chex
 import jax
 import jax.numpy as jnp
+from tokamax._src import shape as shape_lib
 
 
 # pylint: disable=missing-function-docstring
@@ -42,7 +43,9 @@ class FlexAttentionTestBase(absltest.TestCase):
     v = jax.random.normal(rng2, (2, 512, 4, 64))
     bias = jax.random.normal(rng3, (2, 1, 256, 128))
 
-    upscale_bias = lambda x: jnp.repeat(jnp.repeat(x, 2, axis=-2), 4, axis=-1)
+    def upscale_bias(x):
+      with shape_lib.upcast_broadcast():
+        return jnp.repeat(jnp.repeat(x, 2, axis=-2), 4, axis=-1)
 
     with jax.default_matmul_precision("highest"):
       expected = jax.nn.dot_product_attention(q, k, v, bias=upscale_bias(bias))