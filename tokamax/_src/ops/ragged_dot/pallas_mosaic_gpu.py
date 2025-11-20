573e2d81001f41027864546e3c8d48236b259c2b
Giorgio Arena <giorgioa@google.com>
2025-11-20 06:10:52 -0800

    Conform Pallas:MGPU Attention to Pallas:MGPU RaggedDot structure. Forward.
    PiperOrigin-RevId: 834731451

--- tokamax/_src/ops/ragged_dot/pallas_mosaic_gpu.py
+++ tokamax/_src/ops/ragged_dot/pallas_mosaic_gpu.py
@@ -20,7 +20,6 @@ from typing import ClassVar
 import jax
 from jax.extend import backend
 import jax.numpy as jnp
-import qwix
 from tokamax._src import mosaic_gpu as mosaic_gpu_lib
 from tokamax._src import precision as precision_lib
 from tokamax._src import quantization
