99d92e524644ff702b3e8a99f020b3f481d19f4d
Giorgio Arena <giorgioa@google.com>
2025-12-04 07:08:08 -0800

    Add sm100 attention kernel.
    PiperOrigin-RevId: 840229582

--- tokamax/_src/ops/attention/pallas_mosaic_gpu_kernel_sm90.py
+++ tokamax/_src/ops/attention/pallas_mosaic_gpu_kernel_sm90.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flash attention with Mosaic GPU."""
+"""H100 Flash attention with Mosaic GPU."""
 
 import functools
 import math
