6e02a39cfff5237f9e0884679415291696338f8a
Stanley Tzeng <stzeng@google.com>
2025-12-11 09:39:02 -0800

    Fixing Pallas Mosaic GPU Kernel
    PiperOrigin-RevId: 843259844

--- tokamax/_src/ops/attention/pallas_mosaic_gpu_kernel_sm90.py
+++ tokamax/_src/ops/attention/pallas_mosaic_gpu_kernel_sm90.py
@@ -109,7 +109,7 @@ def flash_attention_kernel(
     wg = lax.axis_index("wg")
 
     (
-        ((q_smems, k_smems), (o_smems, *residual_smems)),
+        (q_smems, k_smems, o_smems, *residual_smems),
         v_smems,
         q_barriers,
         bias_smems,
@@ -412,7 +412,7 @@ def flash_attention_kernel(
         lambda *args: kernel(*refs, scoped=args),
         plgpu.RefUnion(
             (q_scratch, k_scratch),
-            (o_scratch, *((l_scratch, m_scratch) if return_residuals else ())),
+            (o_scratch, ((l_scratch, m_scratch) if return_residuals else ())),
         ),
         v_scratch,  # wg1 may still access v as wg0 writes to {o,l,m}_scratch.
         q_barriers,
