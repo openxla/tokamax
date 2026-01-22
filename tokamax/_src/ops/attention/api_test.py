Fix error in handling causal with boolean mask.
PiperOrigin-RevId: 859731810

tokamax/_src/ops/attention/api_test.py
@@ -157,6 +157,7 @@ class DotProductAttentionTest(parameterized.TestCase):
           ('custom', 'padding'),
           ('bias', 'causal'),
           ('causal', 'sliding_window'),
+          ('causal', 'custom')
       ],
   )
   def testDotProductAttentionMask(self, mask_mode):
@@ -183,7 +184,7 @@ class DotProductAttentionTest(parameterized.TestCase):
     dtype = jnp.bfloat16
     cudnn_bias = self.IMPL == 'cudnn' and 'bias' in mask_mode
     B, S, T, N, H = (1 if cudnn_bias else 2), 256, 256, 4, 64
-    if 'custom' in mask_mode:
+    if 'custom' in mask_mode and jax.default_backend() == 'tpu':
       # TODO: Remove once causal + boolean custom mask is
       # supported.
       if 'causal' in mask_mode:

tokamax/_src/ops/attention/pallas_mosaic_tpu.py
@@ -167,11 +167,8 @@ class PallasMosaicTpuFlashAttention(base.DotProductAttention[Config, Key]):
     shard_count = 1
 
     mask_shape = (q_seq_len, kv_seq_len)
-    if mask.is_causal:
-      splash_mask = mask_lib.CausalMask(
-          shape=(q_seq_len, kv_seq_len), shard_count=shard_count
-      )
-    elif mask.bool_mask is not None:
+
+    if mask.bool_mask is not None:
       if mask.is_causal:
         raise NotImplementedError(
             "Causal attention with a boolean mask is not supported."
@@ -186,6 +183,10 @@ class PallasMosaicTpuFlashAttention(base.DotProductAttention[Config, Key]):
       if mask_batch_size != 1:
         raise NotImplementedError("Only unbatched boolean masks are supported.")
       splash_mask = jnp.squeeze(splash_mask, axis=(0, 1))  # (seq_q, seq_kv)
+    elif mask.is_causal:
+      splash_mask = mask_lib.CausalMask(
+          shape=mask_shape, shard_count=shard_count
+      )
     else:
       splash_mask = mask_lib.FullMask(mask_shape)
 