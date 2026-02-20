a625e3dfb70b45c5cb2b6a35e655c81830fa3a08
Sebastian Bodenstein <sbodenstein@google.com>
2026-02-20 04:57:39 -0800

    Refactor pydantic_test.py.
    PiperOrigin-RevId: 872845227

--- tokamax/_src/pydantic_test.py
+++ tokamax/_src/pydantic_test.py
@@ -27,14 +27,18 @@ from tokamax._src import batching
 from tokamax._src import pydantic as pydantic_lib
 from tokamax._src import utils
 from tokamax._src.ops import op as op_lib
+from tokamax._src.ops.attention import arg_specs as attn_arg_specs
 from tokamax._src.ops.attention import base as attn_base
 from tokamax._src.ops.attention import pallas_triton as pl_attn
+from tokamax._src.ops.normalization import arg_specs as norm_arg_specs
 from tokamax._src.ops.normalization import base as norm_base
+from tokamax._src.ops.ragged_dot import arg_specs as ragged_dot_arg_specs
 from tokamax._src.ops.ragged_dot import base as ragged_dot_base
 from tokamax._src.ops.ragged_dot import pallas_triton as pl_ragged_dot
-from tokamax._src.ops.attention import arg_specs as attn_arg_specs
-from tokamax._src.ops.normalization import arg_specs as norm_arg_specs
-from tokamax._src.ops.ragged_dot import arg_specs as ragged_dot_arg_specs
+
+_ATTENTION_ARG_SPECS = attn_arg_specs.ARG_SPECS
+_NORMALIZATION_ARG_SPECS = norm_arg_specs.ARG_SPECS
+_RAGGED_DOT_ARG_SPECS = ragged_dot_arg_specs.ARG_SPECS
 
 
 def _eval_shape(spec):
@@ -194,14 +198,14 @@ class PydanticTest(parameterized.TestCase):
     self.assertEqual(op, op_roundtrip)
 
   @parameterized.named_parameters(
-      ("attention", attn_base.DotProductAttention, attn_arg_specs),
-      ("normalization", norm_base.Normalization, norm_arg_specs),
-      ("ragged_dot", ragged_dot_base.RaggedDot, ragged_dot_arg_specs),
+      ("attention", attn_base.DotProductAttention, _ATTENTION_ARG_SPECS),
+      ("normalization", norm_base.Normalization, _NORMALIZATION_ARG_SPECS),
+      ("ragged_dot", ragged_dot_base.RaggedDot, _RAGGED_DOT_ARG_SPECS),
   )
   def test_arg_specs_roundtrip(self, op_cls, arg_specs):
     spec = pydantic_lib.get_arg_spec_model("ArgSpec", op_cls().signature)
     adapter = pydantic.TypeAdapter(spec)
-    for arg_spec in arg_specs.ARG_SPECS:
+    for arg_spec in arg_specs:
       spec = arg_spec.args
       with self.subTest(arg_spec.full_name):
         spec = op_lib._abstractify(_eval_shape(spec))
