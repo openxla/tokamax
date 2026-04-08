3d3a0fa844aad729efe2a826fb1d15e1434ff791
Chris Jones <cjfj@google.com>
2026-04-08 08:37:32 -0700

    Remove `warp_specialized` option from MGPU ragged dot config.
    PiperOrigin-RevId: 896520659

--- tokamax/_src/ops/ragged_dot/pallas_mosaic_gpu_common.py
+++ tokamax/_src/ops/ragged_dot/pallas_mosaic_gpu_common.py
@@ -54,7 +54,6 @@ class Config:
   split_k: pydantic.PositiveInt
   split_m: pydantic.PositiveInt = 1
   grid_block_n: pydantic.PositiveInt = 1
-  warp_specialized: bool = True
   persistent: bool = True
   post_scale: bool = False
   # B200 collective MMA
