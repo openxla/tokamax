29fa18c4c2a689980d04091ebe707687e727f5af
fdz-1999 <fengdaozhuo1999@163.com>
2026-07-28 21:16:32 +0800

    Add Ant Group copyright notices

--- tokamax/_src/ops/experimental/kda/cp_utils.py
+++ tokamax/_src/ops/experimental/kda/cp_utils.py
@@ -1,4 +1,5 @@
 # Copyright 2025 DeepMind Technologies Limited. All Rights Reserved.
+# Copyright 2026 Ant Group. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
